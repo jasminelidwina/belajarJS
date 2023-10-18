5727c119c3341fd0d95f7f2b2fe0e7489d9e4b5b
Jasmine Lidwina Hanny Hedyana Putri <144589740+jasminelidwina@users.noreply.github.com>
2023-10-18 18:52:35 +0700

    Update main.js

--- main.js
+++ main.js
@@ -28,22 +28,70 @@
 //   document.write("Keep Trying");
 // }
 
-let x = 6;
-let y = 3;
+// let x = 6;
+// let y = 3;
 
-console.log(x < 10 && y > 1);
-console.log(x < 10 && y < 1);
-console.log(x == 5 || y == 5);
-console.log(x == 6 || y == 5);
-console.log(!x == y);
+// console.log(x < 10 && y > 1);
+// console.log(x < 10 && y < 1);
+// console.log(x == 5 || y == 5);
+// console.log(x == 6 || y == 5);
+// console.log(!x == y);
 
-document.write(x < 10 && y > 1);
-document.write("<br>");
-document.write(x < 10 && y < 1);
-document.write("<br>");
-document.write(x == 5 || y == 5);
-document.write("<br>");
-document.write(x == 6 || y == 5);
-document.write("<br>");
-document.write(!x == y);
-document.write("<br>");
+// document.write(x < 10 && y > 1);
+// document.write("<br>");
+// document.write(x < 10 && y < 1);
+// document.write("<br>");
+// document.write(x == 5 || y == 5);
+// document.write("<br>");
+// document.write(x == 6 || y == 5);
+// document.write("<br>");
+// document.write(!x == y);
+// document.write("<br>");
+
+// let p = document.querySelector("p");
+// let button = document.querySelector("button");
+// let input = document.querySelector("input");
+// button = addEventListener("click", function () {
+//   let isi = input.value;
+//   console.log(isi);
+//   p.innerHTML = isi;
+// });
+
+const inputList = document.getElementById("input-list");
+const listInput = document.getElementById("list");
+
+function tambah() {
+  if (inputList.value === "") {
+    alert("isi bang");
+  } else {
+    let li = document.createElement("li");
+    let checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.className = "daftar-list";
+    li.appendChild(checkbox);
+    let namaTugas = document.createTextNode(inputList.value);
+    li.appendChild(namaTugas);
+
+    let tombolHapus = document.createElement("button");
+    tombolHapus.textContent = "x";
+    tombolHapus.className = "tombol-hapus";
+    li.appendChild(tombolHapus);
+    list.appendChild(li);
+
+    inputList.value = "";
+    li.style.listStyleType = "none";
+    tombolHapus.style.listStyleType = "none";
+
+    tombolHapus.addEventListener("click", function () {
+      li.remove();
+    });
+
+    checkbox.addEventListener("change", function () {
+      if (checkbox.checked) {
+        li.style.textDecoration = "line-through";
+      } else {
+        li.style.textDecoration = "none";
+      }
+    });
+  }
+}
